24cd44ca645517fece07ec0756565936dda6a7ab
TRHorse <therunninghorse00@gmail.com>
2021-06-17 01:51:43 -0700

    be motivate

---
+++ index.js
@@ -0,0 +1,31 @@
+const quotes = document.querySelector(".quotes");
+const author = document.querySelector(".author");
+const next = document.querySelector(".next");
+
+let showData = "";
+
+const nextText = () => {
+  let rand = Math.floor(Math.random() * 1640);
+  console.log(rand);
+  const quotesText = showData[rand].text;
+  const quotesAuthor = showData[rand].author;
+
+  
+  quotes.innerHTML = "'' " + quotesText + " ''";
+  quotesAuthor === null
+    ? (author.innerText = "Unknown")
+    : (author.innerText = "BY- " + quotesAuthor);
+};
+
+const getData = async () => {
+  const api = "quotes.json";
+
+  try {
+    let data = await fetch(api);
+    showData = await data.json();
+    nextText();
+  } catch (error) {}
+};
+
+next.addEventListener("click", nextText);
+getData();
