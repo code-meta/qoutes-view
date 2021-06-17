d93486252f7fcedf9866df42f7d0ac6011e8050a
TRHorse <85730196+TRHorse@users.noreply.github.com>
2021-06-17 08:31:33 -0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const quotes = document.querySelector(".quotes");
 const author = document.querySelector(".author");
 const next = document.querySelector(".next");
+const wrapper = document.querySelector(".wrapper");
 
 let showData = "";
 
@@ -10,8 +11,6 @@ const nextText = () => {
   const quotesText = showData[rand].text;
   const quotesAuthor = showData[rand].author;
 
-  console.log(quotesText);
-  console.log(quotesAuthor);
   quotes.innerHTML = "'' " + quotesText + " ''";
   quotesAuthor === null
     ? (author.innerText = "Unknown")
@@ -28,5 +27,38 @@ const getData = async () => {
   } catch (error) {}
 };
 
-next.addEventListener("click", nextText);
+let bg = [
+  "#9a9a1b",
+  "indigo",
+  "#169207",
+  "#232222",
+  "#31174e",
+  "orange",
+  "#ff607b",
+  "purple",
+  "salmon",
+  "cadetblue",
+  "#7e6db3",
+  "#625675",
+  "#3d4669F",
+  "#7f8e30",
+  "#0d5d6f",
+  "#8f8ae0",
+  "#e85685",
+  "#567b4f",
+  "#7dbfce",
+  "#455a5f",
+  "#ffbb66",
+];
+
+console.log(bg.length);
+
+const clsIndxUP = Math.floor(Math.random() * 10);
+wrapper.style.background = bg[clsIndxUP];
+next.addEventListener("click", () => {
+  nextText();
+  const clsIndx = Math.floor(Math.random() * 10);
+  wrapper.style.background = bg[clsIndx];
+});
+
 getData();
